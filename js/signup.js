5fff0962f46868917a6194d1677072df242fd671
한병현 <gksqudgus777@naver.com>
2026-02-07 17:08:12 +0900

    feature: 회원가입 페이지 이메일 확인 함수 구현

---
+++ js/signup.js
@@ -0,0 +1,32 @@
+const emailInput = document.querySelector('#signup-email');
+
+function checkEmail(e) {
+  const REQUIRED_MESSAGE = "이메일을 입력해주세요.";
+  const WRONG_MESSAGE = "잘못된 이메일 형식입니다.";
+  
+  const inputValue = e.target.value.trim();
+
+  const inputWrapper = e.target.closest('.input');
+  const errorInput = inputWrapper.querySelector('.input-main');
+  const errorText = inputWrapper.querySelector('.error-message');
+
+  if(inputValue === ""){
+    errorText.classList.add('wrong-input');
+    errorInput.classList.add('wrong-input');
+    errorText.textContent = REQUIRED_MESSAGE;
+    return;
+  }
+
+  if(!inputValue.includes('@')){
+    errorText.classList.add('wrong-input');
+    errorInput.classList.add('wrong-input');
+    errorText.textContent = WRONG_MESSAGE;
+    return;
+  }
+
+  errorText.classList.remove('wrong-input');
+  errorInput.classList.remove('wrong-input');
+  errorText.textContent = '';
+}
+
+emailInput.addEventListener('focusout', checkEmail);
