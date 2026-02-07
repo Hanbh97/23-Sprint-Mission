58da1bdad6c2fe27bd4eff4358d10061628da998
한병현 <gksqudgus777@naver.com>
2026-02-07 17:12:55 +0900

    feature: 회원가입 페이지 닉네임 확인 함수 구현

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,5 @@
 const emailInput = document.querySelector('#signup-email');
+const nicknameInput = document.querySelector('#signup-nickname');
 
 function checkEmail(e) {
   const REQUIRED_MESSAGE = "이메일을 입력해주세요.";
@@ -29,4 +30,26 @@ function checkEmail(e) {
   errorText.textContent = '';
 }
 
+function checkNickname(e) {
+  const REQUIRED_MESSAGE = "닉네임을 입력해주세요.";
+
+  const inputValue = e.target.value.trim();
+
+  const inputWrapper = e.target.closest('.input');
+  const errorInput = inputWrapper.querySelector('.input-main');
+  const errorText = inputWrapper.querySelector('.error-message');
+
+  if (inputValue === ""){
+    errorText.classList.add('wrong-input');
+    errorInput.classList.add('wrong-input');
+    errorText.textContent = REQUIRED_MESSAGE;
+    return;
+  }
+
+  errorText.classList.remove('wrong-input');
+  errorInput.classList.remove('wrong-input');
+  errorText.textContent = '';
+}
+
 emailInput.addEventListener('focusout', checkEmail);
+nicknameInput.addEventListener('focusout', checkNickname);
